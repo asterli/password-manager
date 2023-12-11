3efbf2fd01d30bc19aa79d9355e57faa48754a2a
aster <asli@scu.edu>
2023-12-10 18:36:21 -0800

    Write encryption logic and remove main password functionality

--- password_manager.py
+++ password_manager.py
@@ -1,8 +1,5 @@
 from cryptography.fernet import Fernet
 
-#ask user for the main password to access the password manager
-main_pwd = input("Enter the main password: ")
-
 '''
 def write_key():
     key = Fernet.generate_key()
@@ -12,19 +9,28 @@ def write_key():
 write_key()
 '''
 
+def load_key():
+    file = open('key.key', 'rb')
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
+
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             user, pwd = data.split("|")
-            print("Username:", user, "| Password:", pwd)
+            print("Username:", user, "| Password:", fer.decrypt(pwd.encode()).decode())
 
 def add():
     user = input("Username: ")
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(user + "|" + pwd + "\n")
+        f.write(user + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
     option = input("What would you like to do? \n Type 'view' if you would like to view existing passwords. \n Type 'add' if you would like to add a new password. \n Type 'q' to quit. \n")

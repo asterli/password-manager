1d7379681f0a4dc649469d5bb4adf1cf31eb0c05
aster <asli@scu.edu>
2023-12-10 18:42:30 -0800

    Implement label for credentials functionality and empty file check

--- password_manager.py
+++ password_manager.py
@@ -1,4 +1,5 @@
 from cryptography.fernet import Fernet
+import os
 
 '''
 def write_key():
@@ -19,18 +20,21 @@ key = load_key()
 fer = Fernet(key)
 
 def view():
+    if os.stat('passwords.txt').st_size == 0:
+        print("No passwords to view!")
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
-            user, pwd = data.split("|")
-            print("Username:", user, "| Password:", fer.decrypt(pwd.encode()).decode())
+            label, user, pwd = data.split("|")
+            print("Credentials for:", label, "| Username:", user, "| Password:", fer.decrypt(pwd.encode()).decode())
 
 def add():
+    label = input("What website/org/company/etc is this password for?: ")
     user = input("Username: ")
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(user + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+        f.write(label + "|" + user + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
 
 while True:
     option = input("What would you like to do? \n Type 'view' if you would like to view existing passwords. \n Type 'add' if you would like to add a new password. \n Type 'q' to quit. \n")

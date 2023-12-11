a5bae6fd176d52a1770436546860ae039e8ee160
aster <asli@scu.edu>
2023-12-10 18:04:03 -0800

    Write function and generate a key to encrypt passwords

--- password_manager.py
+++ password_manager.py
@@ -1,19 +1,30 @@
+from cryptography.fernet import Fernet
+
 #ask user for the main password to access the password manager
-pwd = input("Enter the main password: ")
+main_pwd = input("Enter the main password: ")
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+
+write_key()
+'''
 
 def view():
     with open('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
-            user, passw = data.split("|")
-            print("Username:", user, "| Password:", passw)
+            user, pwd = data.split("|")
+            print("Username:", user, "| Password:", pwd)
 
 def add():
-    username = input("Username: ")
+    user = input("Username: ")
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(username + "|" + pwd + "\n")
+        f.write(user + "|" + pwd + "\n")
 
 while True:
     option = input("What would you like to do? \n Type 'view' if you would like to view existing passwords. \n Type 'add' if you would like to add a new password. \n Type 'q' to quit. \n")
